a7312906cfa281a1cb7077a048a2dec6c598f35f
Anuj Jamwal <Anuj.Jamwal@walmart.com>
2017-05-03 12:11:27 +0530

    Lesson 1 of Yaser Abu-Mostafa's Caltech couse on Machine Learning

--- run.sh
+++ run.sh
@@ -2,7 +2,7 @@
 
 DIR=$(cd "$(dirname "$0")"; pwd)
 
-docker run -i -t -p 8888:8888 -v "/opt/notebooks:${DIR}" continuumio/anaconda3 \
+docker run -i -t -p 8888:8888 -v "${DIR}:/opt/notebooks" continuumio/anaconda3 \
   /bin/bash -c "/opt/conda/bin/conda install jupyter -y --quiet && \
   /opt/conda/bin/jupyter notebook --notebook-dir=/opt/notebooks \
   --ip='*' --port=8888 --no-browser"
